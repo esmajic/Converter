feb70411f6d5ef01e5dab2a4b4142afc165b989c
Ervin Smajic <esmajic@yahoo.com>
2019-04-17 08:43:46 +0200

    zavrsio upis koda u klasu Converter

--- Converter.java
+++ Converter.java
@@ -2,4 +2,33 @@ package Converter;
 
 public class Converter {
 
+	private int a;
+	private int b;
+	
+	Converter(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	@Override
+	public String toString() {
+		return "Converter [varijabla a=" + a + ", varijabla b=" + b + "]";
+	}
+	
 }
