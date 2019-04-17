c1351f2d968749970f7e6b63d7708951cc6e8c34
Ervin Smajic <esmajic@yahoo.com>
2019-04-17 09:13:46 +0200

    morao zenu na posao voziti

--- MethodsConverter.java
+++ MethodsConverter.java
@@ -2,4 +2,50 @@ package Converter;
 
 public class MethodsConverter {
 
+	MethodsConverter() {
+
+	}
+
+	public void kgsToPounds(double userInput) {
+		System.out.println("Conversion from kgs into pounds: ");
+		double pound = userInput * 0.4535923;
+		System.out.println(userInput + " kilograms is " + pound + " pounds.");
+		
+	}
+
+	public void poundsToKgs(double userInput) {
+		System.out.println("Conversion from pounds into kgs: ");
+		double kg = userInput / 0.4535923;
+		System.out.println(userInput + " pounds is " + kg + " kgs.");
+		
+	}
+
+	public void multiplication(int a, int b) {
+		System.out.println("Koliko je " + a + " * " + b + " ?");
+		int rezultat = MainQuiz.input.nextInt();
+		System.out.println(rezultat == (a * b) ? "Odgovor je tacan."
+				: "Odgovor je netacan.");
+	}
+
+	public void division(int a, int b) {
+		System.out.println("Koliko je " + a + " / " + b + " ?");
+		int rezultat = MainQuiz.input.nextInt();
+		System.out.println(rezultat == (a / b) ? "Odgovor je tacan."
+				: "Odgovor je netacan.");
+	}
+
+	public void sqrt(int a) {
+		System.out.println("Koliko je korijen iz " + a + " ?");
+		int rezultat = MainQuiz.input.nextInt();
+		System.out.println(rezultat == (Math.sqrt(a)) ? "Odgovor je tacan."
+				: "Odgovor je netacan.");
+	}
+
+	public void square(int a) {
+		System.out.println("Koliko je " + a + " na kvadrat?");
+		int rezultat = MainQuiz.input.nextInt();
+		System.out.println(rezultat == (Math.pow(a, 2)) ? "Odgovor je tacan."
+				: "Odgovor je netacan.");
+	}
+
 }
