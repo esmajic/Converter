03c8cb040f6f6a498084d26c13b0cc63514f0b92
Ervin Smajic <esmajic@yahoo.com>
2019-04-17 08:54:55 +0200

    zapoceo rad na Main klasi...

--- MainConverter.java
+++ MainConverter.java
@@ -1,9 +1,22 @@
 package Converter;
 
+import java.util.Scanner;
+
 public class MainConverter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("=====================================");
+		System.out.println("             Converter               ");
+		System.out.println("=====================================");
+		System.out.println("Choose converter option:  ");
+		int option = input.nextInt();
+		System.out.println("\nEnter value to be converted:  ");
+		int userInput = input.nextInt();
+		
+		input.close();
 
 	}
 
