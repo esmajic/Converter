97b6b10fe95f4e25aac6c5a8369ad26c53f61add
Ervin Smajic <esmajic@yahoo.com>
2019-04-17 16:37:08 +0200

    zavrsio MethodsConverter klasu

--- MethodsConverter.java
+++ MethodsConverter.java
@@ -6,46 +6,44 @@ public class MethodsConverter {
 
 	}
 
-	public void kgsToPounds(double userInput) {
+	public void poundsToKgs(double userInput) {
 		System.out.println("Conversion from kgs into pounds: ");
 		double pound = userInput * 0.4535923;
 		System.out.println(userInput + " kilograms is " + pound + " pounds.");
 		
 	}
 
-	public void poundsToKgs(double userInput) {
+	public void kgsToPounds(double userInput) {
 		System.out.println("Conversion from pounds into kgs: ");
 		double kg = userInput / 0.4535923;
 		System.out.println(userInput + " pounds is " + kg + " kgs.");
 		
 	}
 
-	public void multiplication(int a, int b) {
-		System.out.println("Koliko je " + a + " * " + b + " ?");
-		int rezultat = MainQuiz.input.nextInt();
-		System.out.println(rezultat == (a * b) ? "Odgovor je tacan."
-				: "Odgovor je netacan.");
+	public void inchesToCms(double userInput) {
+		System.out.println("Conversion from inches into cms: ");
+		double cm = userInput / 0.39370;
+		System.out.println(userInput + " inches is " + cm  + " centimeters.");
+		
 	}
 
-	public void division(int a, int b) {
-		System.out.println("Koliko je " + a + " / " + b + " ?");
-		int rezultat = MainQuiz.input.nextInt();
-		System.out.println(rezultat == (a / b) ? "Odgovor je tacan."
-				: "Odgovor je netacan.");
+	public void cmsToInches(double userInput) {
+		System.out.println("Conversion from cms into inches: ");
+		double inch = userInput * 0.39370;
+		System.out.println(userInput + " centimeters is " + inch + " inches.");
+		
 	}
 
-	public void sqrt(int a) {
-		System.out.println("Koliko je korijen iz " + a + " ?");
-		int rezultat = MainQuiz.input.nextInt();
-		System.out.println(rezultat == (Math.sqrt(a)) ? "Odgovor je tacan."
-				: "Odgovor je netacan.");
+	public void fahrenheitToCelsius(double userInput) {
+		System.out.println("Conversion from fahrenheits into celsius: ");
+		double celsius = userInput * 1.8000 + 32;
+		System.out.println(userInput + " fahrenheits is " + celsius + " celsius.");
 	}
 
-	public void square(int a) {
-		System.out.println("Koliko je " + a + " na kvadrat?");
-		int rezultat = MainQuiz.input.nextInt();
-		System.out.println(rezultat == (Math.pow(a, 2)) ? "Odgovor je tacan."
-				: "Odgovor je netacan.");
+	public void celsiusToFahrenheit(double userInput) {
+		System.out.println("Conversion from celsius into fahrenheits: ");
+		double fahrenheit = (userInput - 32) / 1.8000;
+		System.out.println(userInput + " celsius is " + fahrenheit + " fahrenheit.");
 	}
 
 }
